4cdb7245690b61a74feb58dbd9e6bc2f475e721a
Brendan Dash <me@aiwan.run>
2025-05-18 15:29:20 +0800

    fix: init match

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useIsomorphicLayoutEffect } from "./useIsomorphicLayout";
 
 export function useIsMatchMedia(mediaQueryString: string) {
   const [isMatch, setIsMatch] = useState(false);
 
-  useEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     const mediaQueryList = window.matchMedia(mediaQueryString);
+
+    setIsMatch(mediaQueryList.matches);
 
     const listener = (event: MediaQueryListEvent) => {
       setIsMatch(event.matches);
     };
 
     mediaQueryList.addEventListener("change", listener);
+
     return () => {
       mediaQueryList.removeEventListener("change", listener);
     };
